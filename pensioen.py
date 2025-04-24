8b36cb14ea97a612e4abc407d545c8630c40d3f0
Fabules469 <62789411+Fabules469@users.noreply.github.com>
2025-04-24 18:16:58 +0200

    My second commit

--- pensioen.py
+++ pensioen.py
@@ -1,4 +1,12 @@
 """ Het programma berekent de pensioenleeftijd en het pensioenbedrag van een werknemer."""
+
+
+
+
+
 print("Wat is je leeftijd?")
 leeftijd = int(input("Voer het aantal jaren in: "))
+print("Wat is je werkstatuut?")
+werkstatuut = input("Voer in: medewerker, zelstandige of ambtenaar:").lower()
+
 
